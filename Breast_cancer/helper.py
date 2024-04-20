7963f4434df7154b684062adc6aeb29682b78275
LiZoMark841619 <githubs001@gmail.com>
2024-04-20 10:43:35 +0200

    ok

--- Breast_cancer/helper.py
+++ Breast_cancer/helper.py
@@ -6,26 +6,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 from sklearn.dummy import DummyClassifier
 
 
-def corr_(dataframe) -> list:
-    
-    feats = dataframe.columns.to_list()[:10]
-    matrix = dataframe[feats].corr()
-    selected = set()
-    for i in range(len(matrix.columns)):
-        for j in range(i):
-            if abs(matrix.iloc[i, j]) < 0.7:
-                selected.add(matrix.columns[i])
-    selected = list(selected)
-    return selected
 
-def custom_heat_(dataframe, feats: list) -> plt:
-
-    plt.figure(figsize=[12, 10])
-    sns.heatmap(dataframe[feats].corr(), annot=True, cmap="mako", linewidths=2, linecolor='white')
-    plt.title('Heatmap')
-    plt.show()
-    plt.clf()
-    
 def confusion_(model, X, y):
     X_test, y_test = train_test_split(X, y, test_size=0.30, random_state=50)[1::2]
     threshs = [i*1/10 for i in range(1, 10)]
